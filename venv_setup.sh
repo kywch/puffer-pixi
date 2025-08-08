6bc894f14b548c5f87475a02bfc625701e87dfcd
kywch <choe.kyoung@gmail.com>
2025-08-07 23:23:34 -0700

    comment out LD_LIBRARY_PATH

--- venv_setup.sh
+++ venv_setup.sh
@@ -2,5 +2,5 @@
 
 export PATH="$PIXI_PROJECT_ROOT/.pixi/envs/default/bin:$PATH"
 
-export LD_LIBRARY_PATH="$PIXI_PROJECT_ROOT/.pixi/envs/default/lib:$PIXI_PROJECT_ROOT/.pixi/envs/default/lib/python3.12/site-packages/torch/lib"
+# export LD_LIBRARY_PATH="$PIXI_PROJECT_ROOT/.pixi/envs/default/lib:$PIXI_PROJECT_ROOT/.pixi/envs/default/lib/python3.12/site-packages/torch/lib"
 export CUDA_HOME=$CONDA_PREFIX
